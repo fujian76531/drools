6df4050d4e2c9fc27799f15100f3b85d4d329b21
xiaotao.shi <xiaotao.shi@accenture.com>
2020-05-07 15:02:31 +0800

    demo

---
+++ src/main/java/com/rule/drools/pojo/Person.java
@@ -0,0 +1,13 @@
+package com.rule.drools.pojo;
+
+import lombok.Data;
+
+/**
+ * Created by XiaoTao on 5/7/2020 11:14 AM
+ */
+@Data
+public class Person {
+    private String name;
+    private int age;
+    private String className;
+}
