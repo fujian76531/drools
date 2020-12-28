db5904406d7d69bd8af7b590c237d81e8073ac43
launcher <s.d.launcher@gmail.com>
2020-12-28 23:30:26 +0800

    设计类型及覆盖风险调整

--- src/main/java/com/rule/drools/contract/ContractDemo.java
+++ src/main/java/com/rule/drools/contract/ContractDemo.java
@@ -2,6 +2,7 @@ package com.rule.drools.contract;
 
 import com.newcore.ifrs17.fact.cashflow.*;
 import com.newcore.ifrs17.fact.contract.IFRS17DefCntrgrpAnnuityConverse;
+import com.newcore.ifrs17.fact.contract.IFRS17DefCntrgrpDsntypeAndRkcover;
 import com.newcore.ifrs17.fact.contract.IFRS17DefCntrgrpMinCalUnit;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
@@ -14,11 +15,11 @@ public class ContractDemo {
     public static void main(String[] args){
         KieServices kss = KieServices.Factory.get();
         KieContainer kc = kss.getKieClasspathContainer();
-        KieSession ks = kc.newKieSession("IFRS17DefCntrgrpAnnuityConverse");
+        KieSession ks = kc.newKieSession("IFRS17DefCntrgrpDsntypeAndRkcover");
 
-        IFRS17DefCntrgrpAnnuityConverse demo = new IFRS17DefCntrgrpAnnuityConverse();
+        IFRS17DefCntrgrpDsntypeAndRkcover demo = new IFRS17DefCntrgrpDsntypeAndRkcover();
 
-        demo.setPolCode("_");
+//        demo.setPolCode("_");
 //        demo.setPolName("");
 //        demo.setIsAnnuityConversion("Y");
 //        demo.setPolCodeConverted("oio");
